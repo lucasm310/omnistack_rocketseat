4f70b5a890dd4124652492227dad2c991f25a6a6
Lucas <lucasm310@gmail.com>
2019-08-08 22:52:07 -0300

    Dia 2 - parte 2 - Finalizando web

--- code/frontend/src/pages/Main.js
+++ code/frontend/src/pages/Main.js
@@ -1,8 +1,73 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import './Main.css';
 
-function Main(){
+import api from '../services/api';
+
+import logo from '../assets/logo.svg';
+import dislike from '../assets/dislike.svg';
+import like from '../assets/like.svg';
+
+
+function Main({ match }){
+	const [ users, setUsers ] = useState([]);
+
+	useEffect(()=>{
+		async function loadusers(){
+			const response = await api.get('/devs', {
+				headers: {
+					user: match.params.id,
+				}
+			});
+			setUsers(response.data);
+		};
+		loadusers();
+	}, [match.params.id]);
+
+	async function handleLike(id){
+		const response = await api.post(`/devs/${id}/likes`, null, {
+			headers: { user: match.params.id }
+		}) 
+		setUsers(users.filter(user => user._id !== id));
+	}
+
+	async function handleDislike(id){
+		const response = await api.post(`/devs/${id}/dislikes`, null, {
+			headers: { user: match.params.id }
+		})
+		setUsers(users.filter(user => user._id !== id));
+	}
+
 	return (
-		<h1>Teste</h1>
+		<div className="main-container">
+			<Link to="/">
+				<img src={logo} alt="Logo"/>
+			</Link>
+			{ users.length > 0 ? (
+				<ul>
+					{users.map(user => (
+					<li key={user._id}>
+						<img src={user.avatar} alt={user.name}/>
+						<footer>
+							<strong>{user.name}</strong>
+							<p>{user.bio}</p>
+						</footer>
+						<div className="buttons">
+							<button type="button" onClick={() => handleDislike(user._id)}>
+								<img src={dislike} alt="Dislike"/>
+							</button>
+							<button type="button" onClick={() => handleLike(user._id)}>
+								<img src={like} alt="Like"/>
+							</button>
+						</div>
+					</li>
+					))}
+				</ul>
+			):(
+				<div className="empty">Acabou :/</div>
+			)}
+			
+		</div>
 	);
 }
 
